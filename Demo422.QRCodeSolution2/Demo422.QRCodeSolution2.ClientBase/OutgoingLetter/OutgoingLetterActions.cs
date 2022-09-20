4544a358b8790d1b36b609ae2c058f46030cc16a
DirectumRXUser <directumrxuser@c.ru>
2022-09-20 15:03:35 +0500

    asd [Rx]

--- Demo422.QRCodeSolution2/Demo422.QRCodeSolution2.ClientBase/OutgoingLetter/OutgoingLetterActions.cs
+++ Demo422.QRCodeSolution2/Demo422.QRCodeSolution2.ClientBase/OutgoingLetter/OutgoingLetterActions.cs
@@ -5,7 +5,6 @@ using Sungero.Core;
 using Sungero.CoreEntities;
 using Demo422.QRCodeSolution2.OutgoingLetter;
 using System.IO;
-using System.Data;
 
 namespace Demo422.QRCodeSolution2.Client
 {
@@ -14,9 +13,8 @@ namespace Demo422.QRCodeSolution2.Client
     public override void ConvertToPdf(Sungero.Domain.Client.ExecuteActionArgs e)
     {
       base.ConvertToPdf(e);
-      
       var documentID=_obj.Id;
-      var path=@"\\DOC-QRCODE\files\";
+      var path=@"E:\C#\repos\DirectumRX_beta\DirectumRX_beta\wwwroot\files\";
       var bodyId=Demo422.QRCodeSolution2.Functions.OutgoingLetter.Remote.GetPublicBodyID(_obj);
       path+=bodyId+".pdf";
       StreamReader reader=new StreamReader(_obj.LastVersion.PublicBody.Read());
@@ -27,7 +25,7 @@ namespace Demo422.QRCodeSolution2.Client
           bytes = memstream.ToArray();
       }
       File.WriteAllBytes(path,bytes);
-      
+
     }
 
     public override bool CanConvertToPdf(Sungero.Domain.Client.CanExecuteActionArgs e)
