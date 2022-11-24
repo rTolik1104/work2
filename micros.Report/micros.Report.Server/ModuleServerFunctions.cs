e56de76bd5f09f252b99b65c64d233662f4bb212
DirectumRXUser <directumrxuser@c.ru>
2022-11-24 13:28:30 +0500

    fefe [Rx]

--- micros.Report/micros.Report.Server/ModuleServerFunctions.cs
+++ micros.Report/micros.Report.Server/ModuleServerFunctions.cs
@@ -10,6 +10,8 @@ namespace micros.Report.Server
   {
     [Public,Remote]
     public void SetDataToTasks(string emolyee_name,string department,int completed,int overdue,int well_done,int bad_done,int in_process, int department_id){
+      emolyee_name = emolyee_name.Replace("'","''");
+      department = department.Replace("'","''");
       using (var command = SQL.GetCurrentConnection().CreateCommand())
       {
         command.CommandText = string.Format(Queries.Module.SetDataToTasksTable, emolyee_name,department, completed,overdue,well_done,bad_done,in_process,department_id);
@@ -19,6 +21,7 @@ namespace micros.Report.Server
     
     [Public,Remote]
     public void SetDataToDepartment(string department,int tasksCount, int department_id){
+      department = department.Replace("'","''");
       using (var command = SQL.GetCurrentConnection().CreateCommand())
       {
         command.CommandText = string.Format(Queries.Module.SetDataToDepartmentTable, department, tasksCount,department_id);
