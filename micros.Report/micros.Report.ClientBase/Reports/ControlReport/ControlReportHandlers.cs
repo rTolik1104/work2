ac575d54909c7e8c12bf8586d77090521e652bb3
Raxmatullaev Tolkin Xolmatjon ugli <tolkin.raxmatullaev@micros.uz>
2022-12-02 16:33:34 +0500

    ауы [Rx]

--- micros.Report/micros.Report.ClientBase/Reports/ControlReport/ControlReportHandlers.cs
+++ micros.Report/micros.Report.ClientBase/Reports/ControlReport/ControlReportHandlers.cs
@@ -14,7 +14,7 @@ namespace micros.Report
       var dialog = Dialogs.CreateInputDialog("Параметры отчета");
       
       var startDate=dialog.AddDate("Начальная дата",true,Calendar.Today.AddDays(-30));
-      var endDate=dialog.AddDate("Конечная дата",true,Calendar.Today);
+      var endDate=dialog.AddDate("Конечная дата",true,Calendar.Today.AddDays(1));
       var departments=dialog.AddSelectMany("Подразделения", true, Sungero.Company.Departments.Null);
       var registrJournal=dialog.AddSelect("Группа журналов", true, micros.Report.Groupses.Null);
       
@@ -23,31 +23,49 @@ namespace micros.Report
         e.Cancel=true;
       
       var performers = Sungero.Company.Employees.GetAll(x=>departments.Value.Contains(x.Department));
-      var documents=new List<int>();
+      var documentIds=new List<int>();
       foreach(var journal in registrJournal.Value.GroupRegistration)
       {
-        var document=Sungero.Docflow.OfficialDocuments.GetAll(x=>x.DocumentRegister==journal.GroupName).Select(d=>d.Id);
-        documents.AddRange(document);
+        var document=Sungero.Docflow.OfficialDocuments.GetAll(x => x.DocumentRegister.Id == journal.GroupName.Id).Select(d => d.Id);
+        documentIds.AddRange(document);
       }
 
       foreach(var department in departments.Value)
       {
-        var performersId = Sungero.Company.Employees.GetAll(x=>x.Department==department).Select(x=>x.Id);
-        var a = Sungero.Workflow.Assignments.GetAll(x => performersId.Contains(x.Performer.Id));
-        var b = a.Where(c => c.Created>=startDate.Value && c.Created<=endDate.Value && c.Attachments.Count != 0);
-        var assignmentsCount = b.Where(v => v.AllAttachments.Any(d => documents.Contains(d.Id))).Count();
+        var performersId = Sungero.Company.Employees.GetAll(x => x.Department.Id == department.Id).Select(x => x.Id);
+        var assigments = Sungero.Workflow.Assignments.GetAll(x => performersId.Contains(x.Performer.Id));
+        var assignments1 = assigments.Where(x => x.Created >= startDate.Value && x.Created <= endDate.Value);
+        var assigments2 = assignments1.Select(t => t.Task);
+        var assignmentsCount = assigments2.Cast<Sungero.Workflow.ITask>()
+                                      .Where(t => t.AttachmentDetails.Any(d => documentIds.Contains(d.AttachmentId.GetValueOrDefault()))).Count();
+        
         Report.Functions.Module.Remote.SetDataToDepartment(department.Name, assignmentsCount, department.Id);
       }
 
       foreach(var performer in performers)
       {
-        var compltete=Sungero.Workflow.Assignments.GetAll(x => x.Performer==performer).Where(s=>s.Status.Value.ToString()=="Completed").Count();
-        var process=Sungero.Workflow.Assignments.GetAll(x => x.Performer==performer).Where(s=>s.Status.Value.ToString()=="InProcess" && s.Deadline>=Calendar.Today && s.Attachments.Any() && documents.Contains(s.Attachments.First().Id)).Count();
-        var overd=Sungero.Workflow.Assignments.GetAll(x => x.Performer==performer).Where(s=>s.Status.Value.ToString()=="InProcess" && s.Deadline<Calendar.Today && s.Attachments.Any() && documents.Contains(s.Attachments.First().Id)).Count();
-        var well=Sungero.Workflow.Assignments.GetAll(x => x.Performer==performer).Where(s=>s.Status.Value.ToString()=="Completed" && s.Completed<=s.Deadline && s.Attachments.Any() && documents.Contains(s.Attachments.First().Id)).Count();
-        var bad=Sungero.Workflow.Assignments.GetAll(x => x.Performer==performer).Where(s=>s.Status.Value.ToString()=="Completed" && s.Completed>s.Deadline && s.Attachments.Any() && documents.Contains(s.Attachments.First().Id)).Count();
+        var assigments = Sungero.Workflow.Assignments.GetAll(x => x.Performer.Id == performer.Id);
         
-        Report.Functions.Module.Remote.SetDataToTasks(performer.Name,performer.Department.Name,compltete,overd,well,bad,process, performer.Department.Id);
+        var compltete=assigments.Where(s => s.Status.Value.ToString() == "Completed").Count();
+        
+        var inProcess = assigments.Where(s => s.Status.Value.ToString() == "InProcess" && s.Deadline>=Calendar.Today || s.Deadline == null)
+                                 .Select(t => t.Task)
+                                 .Cast<Sungero.Workflow.ITask>()
+                                 .Where(t => t.AttachmentDetails.Any(d => documentIds.Contains(d.AttachmentId.GetValueOrDefault()))).Count();
+        var overdue = assigments.Where(s => s.Status.Value.ToString() == "InProcess" && s.Deadline < Calendar.Today)
+                                .Select(t => t.Task)
+                                .Cast<Sungero.Workflow.ITask>()
+                                .Where(t => t.AttachmentDetails.Any(d => documentIds.Contains(d.AttachmentId.GetValueOrDefault()))).Count();
+        var wellDone = assigments.Where(s => s.Status.Value.ToString() == "Completed" && s.Completed <= s.Deadline)
+                                .Select(t => t.Task)
+                                .Cast<Sungero.Workflow.ITask>()
+                                .Where(t => t.AttachmentDetails.Any(d => documentIds.Contains(d.AttachmentId.GetValueOrDefault()))).Count();
+        var badDone = assigments.Where(s => s.Status.Value.ToString() == "Completed" && s.Completed>s.Deadline)
+                                .Select(t => t.Task)
+                                .Cast<Sungero.Workflow.ITask>()
+                                .Where(t => t.AttachmentDetails.Any(d => documentIds.Contains(d.AttachmentId.GetValueOrDefault()))).Count();
+        
+        Report.Functions.Module.Remote.SetDataToTasks(performer.Name, performer.Department.Name, compltete, overdue, wellDone, badDone, inProcess, performer.Department.Id);
       }
         
       ControlReport.StartDate=startDate.Value.Value;
