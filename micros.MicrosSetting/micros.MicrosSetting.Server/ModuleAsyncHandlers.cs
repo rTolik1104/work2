aed0eddb6cf95708804d4fae267a5aa1a193db96
Raxmatullaev Tolkin Xolmatjon ugli <tolkin.raxmatullaev@micros.uz>
2022-12-14 14:04:42 +0500

    Импорт пакета разработки [Rx]

--- micros.MicrosSetting/micros.MicrosSetting.Server/ModuleAsyncHandlers.cs
+++ micros.MicrosSetting/micros.MicrosSetting.Server/ModuleAsyncHandlers.cs
@@ -8,6 +8,26 @@ namespace micros.MicrosSetting.Server
 {
   public class ModuleAsyncHandlers
   {
+    public virtual void CheckCities(micros.MicrosSetting.Server.AsyncHandlerInvokeArgs.CheckCitiesInvokeArgs args)
+    {
+      var cityDataList = DirectoryCity.City.GetCityDataList();
+      var cityExist = micros.DrxUzbekistan.PublicFunctions.City.Remote.GetAllCities();
+      
+      foreach(var city in cityDataList)
+      {
+        var region = micros.DrxUzbekistan.PublicFunctions.Region.Remote.GetAllRegions().Where(r => r.Indexmicros == city.RegionIndex).FirstOrDefault();
+        var country = micros.DrxUzbekistan.PublicFunctions.Country.Remote.GetAllCountries().Where(c => c.Indexmicros == city.CountryIndex).FirstOrDefault();
+        if(cityExist.Where(c => c.Name == city.Name).Count() == 0 && country != null && region != null)
+        {
+          var newCity = micros.DrxUzbekistan.PublicFunctions.City.Remote.CreateCity();
+          newCity.Name = city.Name;
+          newCity.Status = micros.DrxUzbekistan.City.Status.Active;
+          newCity.Region = region;
+          newCity.Country = country;
+          newCity.Save();
+        }
+      }
+    }
 
   }
 }
